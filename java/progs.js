230fe09c99bdc4aad8b09e6086cfde1333c47cfb
Ahmed-hub-boop <60401523+Ahmed-hub-boop@users.noreply.github.com>
2024-04-26 02:56:06 +0300

    Add files via upload

---
+++ java/progs.js
@@ -0,0 +1,39 @@
+let progs = [
+    {
+        name: "X-O Game",
+        img_src: "proj media/Capture-1.PNG",
+        prog_link: "##"
+    },
+
+    {
+        name: "To Do List",
+        img_src: "proj media/Capture-2.PNG",
+        prog_link: "##"
+    },
+    {
+        name: "Sign Up Page",
+        img_src: "proj media/Capture-3.PNG",
+        prog_link: "##"
+    },
+    {
+        name: "WhatsApp",
+        img_src: "proj media/Capture-4.PNG",
+        prog_link: "##"
+    },
+    {
+        name: "WA",
+        img_src: "proj media/Capture-5.PNG",
+        prog_link: "##"
+    },
+
+    {
+        name: "Youtube",
+        img_src: "proj media/Capture-6.PNG",
+        prog_link: "##"
+    },
+    {
+        name: "Facebook",
+        img_src: "proj media/Capture-7.PNG",
+        prog_link: "##"
+    }
+]
